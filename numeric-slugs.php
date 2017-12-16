1dd30d2a6fa1ac31b59a7b7018d2c7174eb1a948
EC2 Default User <ec2-user@ip-172-31-21-203.us-west-2.compute.internal>
2017-12-16 05:35:03 -0800

    init

---
+++ numeric-slugs.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+Plugin Name: Enable Numeric Slugs
+Plugin URI: http://wordpress.org/plugins/enable-numeric-slugs/
+Description: Fix postname(slug) can not be a 4 digit number issue on wordpress 4.9.1. As a matter of fact, its not a bug, wordpress reserve 4 digit number in url for yearly archive. I just simply remove the rewrite rules stored in MySql. If you want to reset to orignal rewrite rules, go to Permalink Settings and Save Changes. That's it..
+Version: 1.0.0
+Author: Yuan Yang
+Author URI: https://84361749.com/
+License: GPL2
+*/
+
+/*  Copyright 2017  aYangYuan  (email : ayangyuan@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+// Force a flush of the rewrite rules when this plugin is activated
+function allow_numeric_slugs_activate() {
+    global $wp_rewrite;
+    $wp_rewrite->flush_rules();
+}
+
+register_activation_hook(__FILE__, 'allow_numeric_slugs_activate');
+function Enable_Numberic_Slug($rules){
+    foreach ($rules as $rule => $rewrite) {
+        if ( preg_match('/\{4\}/',$rule) ) {
+            unset($rules[$rule]);
+        }
+    }
+    return $rules;
+}
+add_filter('rewrite_rules_array', 'Enable_Numberic_Slug');
---
+++ readme.txt
@@ -0,0 +1,24 @@
+=== Enable Numeric Slugs ===
+Author URL: https://84361749.com/
+Github: https://84361749.com/
+Tags: numeric, slugs, postname, number, title, post
+Requires at least: 4.9.1
+Tested up to: 4.9.1
+Stable tag: trunk
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Use post titles and slugs made of only numbers.
+
+== Description ==
+
+Description: Fix postname(slug) can not be a 4 digit number issue on wordpress 4.9.1. As a matter of fact, its not a bug, wordpress reserve 4 digit number in url for yearly archive. I just simply remove the rewrite rules stored in MySql. If you want to reset to orignal rewrite rules, go to Permalink Settings and Save Changes. That's it..
+
+== Installation ==
+
+Install, Activate, and Forget. 
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release
