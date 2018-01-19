d761429084d7d1451c4d47072ec5fc9f35f6da29
root <ayangyuan@gmail.com>
2018-01-18 19:53:51 -0800

    for test

--- numeric-slugs.php
+++ numeric-slugs.php
@@ -1,31 +1,28 @@
 <?php
+/**
+ * Enable Numeric Slugs 
+ *
+ * Enable Numeric Slugs plugin fixes postname(slug) can not be a 4 digit number issue on wordpress 4.9.1. As a matter of fact, its not a bug, wordpress reserve 4 digit number in url for yearly archive. I just simply remove the rewrite rules stored in MySql. If you want to reset to orignal rewrite rules, go to Permalink Settings and Save Changes. That's it..
 
-/*
-Plugin Name: Enable Numeric Slugs
-Plugin URI: http://wordpress.org/plugins/enable-numeric-slugs/
-Description: Fix postname(slug) can not be a 4 digit number issue on wordpress 4.9.1. As a matter of fact, its not a bug, wordpress reserve 4 digit number in url for yearly archive. I just simply remove the rewrite rules stored in MySql. If you want to reset to orignal rewrite rules, go to Permalink Settings and Save Changes. That's it..
-Version: 1.0.0
-Author: Yuan Yang
-Author URI: https://84361749.com/
-Github: https://github.com/ayangyuan/Wordpress-Plugin-Enable-Numeric-Slugs
-License: GPL2
-*/
-
-/*  Copyright 2017  aYangYuan  (email : ayangyuan@gmail.com)
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as
-    published by the Free Software Foundation.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+ * @package   Enable Numeric Slugs
+ * @author    Mr.ING <ayangyuan@gmail.com>
+ * @license   GPL-2.0+
+ * @link      https://squaredaway.studio/wordpress-plugin-enable-numeric-slugs/
+ * @copyright 1999-2018 
+ *
+ * @wordpress-plugin
+ * Plugin Name: Enable Numeric Slugs
+ * Plugin URI:  https://wordpress.org/plugins/enable-numeric-slugs/
+ * GitHub URI:  https://github.com/ayangyuan/Wordpress-Plugin-Enable-Numeric-Slugs
+ * Author URI:  https://squaredaway.studio/wordpress-plugin-enable-numeric-slugs/
+ * Author:      Mr.ING 
+ * Version:     1.0.1
+ * Text Domain: enable-numeric-slugs
+ * Domain Path: /res/lang
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Description: Display the current memory usage bar on admin header.
+ */
 
 // Force a flush of the rewrite rules when this plugin is activated
 function allow_numeric_slugs_activate() {
@@ -43,3 +40,23 @@ function Enable_Numberic_Slug($rules){
     return $rules;
 }
 add_filter('rewrite_rules_array', 'Enable_Numberic_Slug');
+
+
+/** Add links to the plugin action row. */
+//if ( ! defined( 'MR_ING_ENS_PLUGIN_FILE' ) ) {define( 'MR_ING_ENS_PLUGIN_FILE', __FILE__ );}
+if (!function_exists('mr_ing_plugin_row_meta')) {
+function mr_ing_plugin_row_meta( $links, $file ) {
+  //if ( plugin_basename( MR_ING_ENS_PLUGIN_FILE ) === $file ) {
+  if ( plugin_basename( __FILE__ ) === $file ) {
+    $new_links = array(
+    'support'    => '<a href = "http://wordpress.org/support/plugin/enable_numeric_slugs">' . __( 'Support' ) . '</a>',
+    'donate'     => '<a href = "https://squaredaway.studio/donate/">' . __( 'Donate') . '</a>',
+    'contribute' => '<a href = "https://github.com/ayangyuan/Wordpress-Plugin-Enable-Numeric-Slugs">' . __( 'Contribute' ) . '</a>',
+     );
+     $links = array_merge( $links, $new_links );
+   }
+   return $links;
+}
+}
+add_filter( 'plugin_row_meta', 'mr_ing_plugin_row_meta', 10, 2 );
+
--- readme.txt
+++ readme.txt
@@ -1,24 +1,73 @@
 === Enable Numeric Slugs ===
-Author URL: https://84361749.com/
+Contributors: Mr.ING
+Author URI: https://squaredaway.studio/wordpress-plugin-enable-numeric-slugs/
+Plugin URI: https://wordpress.org/plugins/enable-numeric-slugs/ 
 Github: https://github.com/ayangyuan/Wordpress-Plugin-Enable-Numeric-Slugs
+Donate link: https://squaredaway.studio/donate
 Tags: numeric, slugs, postname, number, title, post
-Requires at least: 4.9.1
-Tested up to: 4.9.1
 Stable tag: trunk
-License: GPLv2
+Tested up to: 4.9
+License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
 Use post titles and slugs made of only numbers.
 
 == Description ==
 
-Description: Fix postname(slug) can not be a 4 digit number issue on wordpress 4.9.1. As a matter of fact, its not a bug, wordpress reserve 4 digit number in url for yearly archive. I just simply remove the rewrite rules stored in MySql. If you want to reset to orignal rewrite rules, go to Permalink Settings and Save Changes. That's it..
+[Enable Numeric Slugs](https://squaredaway.studio/wordpress-plugin-enable-numeric-slugs/) plugin fixes postname(slug) can not be a 4 digit number issue on wordpress 4.9.1. As a matter of fact, its not a bug, wordpress reserve 4 digit number in url for yearly archive. I just simply remove the rewrite rules stored in MySql. If you want to reset to orignal rewrite rules, go to Permalink Settings and Save Changes. That's it..
+
+= Key features =
+
+* **Numeric Slugs**: As it.
+* **IAF**: This plug is prefer to work sliently, that once you Installed, Activated, then you will Forget it exist.
+* **Green**: Nothing added to your Wordpress but only pure function.
+
+= Donations =
+
+Would you like to buy me a cup of coffee, like friends hang out at Starbucks someday in the afternoon.
+
+= Contribute =
+
+This plug is also available on [Github](https://github.com/ayangyuan/Wordpress-Plugin-Enable-Numeric-Slugs).
+So, if you've got some cool feature that you'd like to implement into the plugin or a bug you've been able to fix, consider forking the project and sending me a pull request.
 
 == Installation ==
 
-Install, Activate, and Forget. 
+= WordPress install (The easy way) =
+
+1. Navigate to Plugins within your WordPress Admin Area
+
+2. Click "Add new" and in the search box enter "Enable Numeric Slugs"
+
+3. Find the plugin in the list and click "Install Now"
+
+= Manual install =
+
+1. Download the plugin
+
+2. Extract the contents of .zip to wp-content/plugins/ folder.
+
+3. Activate the Plugin in WP-Admin under the Plugins screen
+
+== Screenshots ==
+
+1. Don't worry just "Plug and Play".
+
+== Frequently Asked Questions ==
+
+Any issues regarding to this pluging? feel free to ask me at the [WordPress.org support forum](http://wordpress.org/support/plugin/enable-numeric-slugs)
+
+= What is the first question gonna be like? =
+I have no idea.
 
 == Changelog ==
 
+= 1.0.1 =
+
+* Released:
+        * Add screenshot, logo and banner.
+        * Write readme.txt
+
 = 1.0.0 =
-* Initial release
+
+* Initialized 
